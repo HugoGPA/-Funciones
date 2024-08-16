84a714b7229946aea4c3f82e35068565bbe443a8
HugoGPA <A01711814@tec.mx>
2024-08-16 23:06:33 +0000

    avance2

--- main.cpp
+++ main.cpp
@@ -22,23 +22,51 @@ int sumaRecursiva(int n){
 
 int sumaDirecta(int n) {
     return n * (n + 1) / 2;
+
+}
+
+
+//SUMA RECURSIVA 3n+1 
+
+int OTresNMasUnoRecursiva(int n) {
+   if (n == 1) {
+        return 0;
+    } else if (n % 2 == 0) {
+        return 1 + OTresNMasUnoRecursiva(n / 2);
+    } else {
+        return 1 + OTresNMasUnoRecursiva(3 * n + 1);
+    }
+}
+
+int OTresNMasUnoIterativa(int n) {
+    int pasos = 0;
+    while (n != 1) {
+        n = 3 * n + 1;
+        ++pasos;
+    }
+    return pasos;
 }
 
 int main() {
     int n;
-    
-    cout << "Ingrese un entero positivo n: ";
+
+    cout << "Ingresa n: ";
     cin >> n;
 
     
     cout << " metodo iterativo: " << sumaIterativa(n) << endl;
     cout << " metodo recursivo: " << sumaRecursiva(n) << endl;
     cout << "metodo directo: " << sumaDirecta(n) << endl;
+    cout << " pasos usando el metodo recursivo: " << OTresNMasUnoRecursiva(n) << endl;
+    cout << " pasos usando el metodo iterativo: " << OTresNMasUnoIterativa(n) << endl;
 
     return 0;
 }
 
- 
+
+
+
+
     
   
     
